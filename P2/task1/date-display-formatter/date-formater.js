2bd4a1b598673c90318cee5fa0500051d451c08e
snovis <sergeygoodgood@gmail.com>
2020-11-01 23:59:09 +0300

    was added frame of date-formater

--- P2/task1/date-display-formatter/date-formater.js
+++ P2/task1/date-display-formatter/date-formater.js
@@ -1,4 +1,6 @@
-var executeBut = document.getSelection("");
+var executeBut = document.querySelector("#executer");
+var options = document.querySelector("#options");
+var input1 = document.getElementById("input1");
 
 formater = {
     format: function(){
@@ -11,4 +13,18 @@ executer = {
     }
 }
 
-executerBut.addEventListener("click", executer.format);
+function optionsChanged(){
+    console.log("options was changet");
+    if(+options.value > 2){
+        console.log("more 2");
+        input1.style.display = "block";
+    }
+    else{
+        input1.style.display = "none";
+    }
+}
+
+console.log(options);
+executeBut.addEventListener("click", executer.format);
+options.addEventListener("change", optionsChanged);
+
