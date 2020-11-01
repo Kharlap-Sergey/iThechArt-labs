feac3bdb387cd5f04995a2c1aafd012607a4050f
snovis <sergeygoodgood@gmail.com>
2020-11-01 23:07:08 +0300

    was ended cache calculator

---
+++ P2/task1/caching-calculator/calculator.js
@@ -0,0 +1,63 @@
+var executeBut = document.querySelector("#executer");
+var numberA = document.querySelector("#input1");
+var numberB = document.querySelector("#input2");
+var operator = document.querySelector("#operation");
+
+cacheCalculator = {
+    cache: {},
+    getOrSetCache: function (val1, val2, operatino, ...args) {
+        var cacheKey = operatino + val1 + val2;
+
+        if (!args.length) {
+            return this.cache[cacheKey];
+        }
+
+        this.cache[cacheKey] = args[0];
+
+    },
+
+    executeOperation: function (val1, val2, opearation) {
+        var val1 = parseFloat(val1);
+        var val2 = parseFloat(val2);
+        var res;
+        switch (opearation) {
+            case '+':
+                res = val1 + val2;
+                break;
+            case '-':
+                res = val1 - val2;
+                break;
+            case '*':
+                res = val1 * val2;
+                break;
+            case "/":
+                res = val1 / val2;
+                break;
+        }
+
+        return res;
+    },
+
+    calculate: function (val1, val2, opearation) {
+        var result = this.getOrSetCache(val1, val2, opearation);
+
+        if(!result){
+            var result = this.executeOperation(val1, val2, opearation);
+            this.getOrSetCache(val1, val2, opearation, result);
+        }
+
+        return result;
+    },
+}
+
+executer = {
+    calculate: function() {
+        console.log("executin was started");
+        var result = cacheCalculator.calculate(numberA.value, numberB.value, operator.value);
+        alert("result is " + result);
+    }
+}
+
+console.log(executeBut);
+executeBut.addEventListener("click", executer.calculate);
+
