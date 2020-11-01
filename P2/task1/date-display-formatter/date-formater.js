069e98a7bb001d30aaaec174b1adafd404982b44
snovis <sergeygoodgood@gmail.com>
2020-11-01 22:07:56 +0300

    smoe prepearing for task

---
+++ P2/task1/date-display-formatter/date-formater.js
@@ -0,0 +1,14 @@
+var executeBut = document.getSelection("");
+
+formater = {
+    format: function(){
+        
+    }
+}
+executer = {
+    format: function () {
+        
+    }
+}
+
+executerBut.addEventListener("click", executer.format);
