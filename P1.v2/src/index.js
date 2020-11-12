0ebf5a3a2b06e287d85cb8402e7c63ff00e2497f
Siarhei Kharlap <sergeygoodgood@gmail.com>
2020-11-12 17:01:19 +0300

    footer was added

--- P1.v2/src/index.js
+++ P1.v2/src/index.js
@@ -2,3 +2,4 @@ import "./styles/header/style.scss";
 import "./styles/style.scss";
 import "./styles/baner/style.scss";
 import "./styles/featured-content/style.scss";
+import "./styles/footer/style.scss";
