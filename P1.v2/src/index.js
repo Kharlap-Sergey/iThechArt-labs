5d055bb145dd66852d0e3c0b074ef3645cea309d
Siarhei Kharlap <sergeygoodgood@gmail.com>
2020-12-02 15:34:07 +0300

    remove old projects

--- P1.v2/src/index.js
+++
@@ -1,6 +0,0 @@
-import "./styles/header/style.scss";
-import "./styles/style.scss";
-import "./styles/baner/style.scss";
-import "./styles/featured-content/style.scss";
-import "./styles/footer/style.scss";
-import "./styles/main/style.scss";
