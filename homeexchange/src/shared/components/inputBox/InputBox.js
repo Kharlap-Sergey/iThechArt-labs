c0b3ae06409e07e6fb70136875e814ca56d56b47
snovis <sergeygoodgood@gmail.com>
2020-12-06 13:26:25 +0300

    replace the document.querySelector to the React. ef

--- homeexchange/src/shared/components/inputBox/InputBox.js
+++ homeexchange/src/shared/components/inputBox/InputBox.js
@@ -5,13 +5,15 @@ class InputBox extends React.PureComponent {
     super(props);
     this.onChange = this.onChange.bind(this);
     this.show_hide_password = this.show_hide_password.bind(this);
+
+    this.refToInput = React.createRef();
   }
 
   show_hide_password(event) {
     event.preventDefault();
     console.log(event);
-    var input = document.querySelector('[name=password]');
-    if (input.getAttribute('type') == 'password') {
+    var input = this.refToInput.current;
+    if (input.getAttribute('type') === 'password') {
       event.target.classList.add("view");
       input.setAttribute('type', 'text');
     } else {
@@ -32,6 +34,7 @@ class InputBox extends React.PureComponent {
     return (
       <div className="input-box">
         <input
+          ref={this.refToInput}
           className="input-box__input"
           type={this.props.typ ?? "text"}
           name={this.props.name.toString()}
@@ -40,7 +43,7 @@ class InputBox extends React.PureComponent {
           onChange={this.onChange}
           {...this.props.validationAttributes}
         />
-        {this.props.name == "password" && (
+        {this.props.name === "password" && (
           <a
             href="#"
             className="password-control"
