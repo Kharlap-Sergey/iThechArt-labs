1534e14d9c831f59fe2c0500bd2477e8f32d59e6
Siarhei Kharlap <sergeygoodgood@gmail.com>
2020-12-08 09:43:25 +0300

    request wrapper does not need token param anymore

--- homeexchange/src/shared/utils/requestWrapper.js
+++ homeexchange/src/shared/utils/requestWrapper.js
@@ -1,35 +1,38 @@
+import { auth } from "./auth";
 export const requestWrapper = {
-  get: (url, token="") => {
+  getToken: () => {
+    return auth.getToken() ?? "";
+  },
+  get: (url) => {
     const options = {
       method: "GET",
       headers: {
         Accept: "application/json",
-        Authorization: "Bearer " + token,
+        Authorization: "Bearer " + requestWrapper.getToken(),
       },
-    }
+    };
     return fetch(url, options);
   },
-  post: (url, dataJson, token="") => {
+  post: (url, dataJson) => {
     const options = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
-        Authorization: "Bearer " + token,
+        Authorization: "Bearer " + requestWrapper.getToken(),
       },
       body: JSON.stringify(dataJson),
-    }
-    return  fetch(url, options);
+    };
+    return fetch(url, options);
   },
-  deleteByIdQueryParam:
-  (url,token="") => {
+  deleteByIdQueryParam: (url) => {
     const options = {
       method: "DELETE",
       headers: {
         Accept: "application/json",
-        Authorization: "Bearer " + token,
+        Authorization: "Bearer " + requestWrapper.getToken(),
       },
-    }
-    return  fetch(url, options);
+    };
+    return fetch(url, options);
   },
-}
+};
