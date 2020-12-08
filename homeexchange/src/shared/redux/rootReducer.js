4ad5badfce421418e7acd6ec2b802f08e57b9a5a
Siarhei Kharlap <sergeygoodgood@gmail.com>
2020-12-08 12:29:37 +0300

    change the account reducer

--- homeexchange/src/shared/redux/rootReducer.js
+++ homeexchange/src/shared/redux/rootReducer.js
@@ -1,14 +1,14 @@
 import { combineReducers } from "redux";
-import { FormReducer } from "../../redux/FormReducer";
 import { reducer as toastrReducer } from "react-redux-toastr";
 import { adReducer } from "./ad/adReducer";
 import { redirectReducer } from "./redirect/redirectReducer";
 import { remoteInteractionReducer } from "./remoteInteraciton/remoteInteractionReducer";
 import { profileReducer } from "./profile/profileReducer";
 import { adsPageListReducer } from "../components/adsPageList/redux/adsPageListReducer";
+import { accountReducer } from './account/accountReducer';
 
 export const rootReducer = combineReducers({
-  user: FormReducer,
+  user: accountReducer,
   ads: adReducer,
   toastr: toastrReducer,
   redirect: redirectReducer,
