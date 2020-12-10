da203a6f59803f42464f24a695e61a43acfbbc19
Siarhei Kharlap <sergeygoodgood@gmail.com>
2020-12-10 09:50:22 +0300

    add chat entities

---
+++ homeexchange/server/serverApi/Domain/Entities/ChatMember.cs
@@ -0,0 +1,20 @@
+﻿using serverApi.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace serverApi.Domain.Entities
+{
+    public sealed class ChatMember
+    {
+
+        [ForeignKey("ChatId")]
+        [Required]
+        public int ChatId { set; get; }
+        public Chat Chat { get; set; }
+
+        [ForeignKey("UserId")]
+        [Required]
+        public int UserId { set; get; }
+        public User User { get; set; }
+    }
+}
---
+++ homeexchange/server/serverApi/Domain/Entities/ChatMessage.cs
@@ -0,0 +1,26 @@
+﻿using serverApi.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace serverApi.Domain.Entities
+{
+    public sealed class ChatMessage
+    {
+        [Key]
+        public int Id { set; get; }
+
+        [ForeignKey("ChatId")]
+        [Required]
+        public int ChatId { set; get; }
+        public Chat Chat { get; set; }
+
+        [ForeignKey("UserId")]
+        [Required]
+        public int UserId { set; get; }
+        public User User { get; set; }
+
+        public string Content { set; get; }
+        public DateTime PublicationDate{set; get;}
+    }
+}
