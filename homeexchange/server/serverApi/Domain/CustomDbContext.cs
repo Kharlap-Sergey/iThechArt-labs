b9db743ada12a050584640593a79ced4465a3778
Siarhei Kharlap <sergeygoodgood@gmail.com>
2020-12-10 10:23:10 +0300

    index on homeexchange: da203a6 add chat entities

--- homeexchange/server/serverApi/Domain/CustomDbContext.cs
+++ homeexchange/server/serverApi/Domain/CustomDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using serverApi.Domain.Entities;
 using serverApi.Models;
 using System;
 using System.Collections.Generic;
@@ -12,7 +13,7 @@ namespace serverApi.Domain
         public CustomDbContext(DbContextOptions<CustomDbContext> options)
             : base(options)
         {
-           // Database.EnsureDeleted();
+            //Database.EnsureDeleted();
             Database.EnsureCreated();   // создаем базу данных при первом обращении
         }
 
@@ -23,13 +24,21 @@ namespace serverApi.Domain
                 relationship.DeleteBehavior = DeleteBehavior.Restrict;
             }
 
+            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+            modelBuilder.Entity<User>().HasIndex(u => u.Nickname).IsUnique();
+            modelBuilder.Entity<ChatMember>().HasKey(e => new { e.ChatId, e.UserId });
+
             base.OnModelCreating(modelBuilder);
-            //modelBuilder.Entity<User>();
         }
         public DbSet<User> Users { get; set; }
         public DbSet<ResponseToAd> ResponsesToAd { get; set; }
         public DbSet<Ad> Ads { get; set; }
         public DbSet<NotificationAboutResponseToAd> NotificationsAboutResponseToAd { get; set; }
+
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<ChatMember> ChatMembers { get; set; }
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+
         //public DbSet<Comment> Comments { get; set;}
         //public DbSet<Lang> Langs { get; set;}
     }
