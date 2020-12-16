84c0da14d67e38fd9ad2098a0806f3d7184c9c9f
snovis <sergeygoodgood@gmail.com>
2020-12-16 21:48:41 +0300

    change profile editor

--- homeexchange/src/editProfile/EditProfile.js
+++ homeexchange/src/editProfile/EditProfile.js
@@ -5,7 +5,8 @@ import AccountForm from "../shared/components/accountForm/AccoutForm";
 import { inputAttributes } from '../shared/utils/inputArguments';
 import { updateUserPost } from './redux/updateUserActionCreator';
 import Loader from '../shared/components/Loader/Loader';
-
+import bgImg from "../shared/imgs/repairing.svg"
+import "./edit-profile.scss";
 class EditProfile extends PureComponent {
   constructor(props) {
     super(props);
@@ -39,7 +40,7 @@ class EditProfile extends PureComponent {
   render() {
     console.log(this.props);
     return (
-      <div className="profile">
+      <div className="edit-profile">
         {/* <div className="profile__inf">
           <AccountIformation {...this.props.profile}>
             {this.state.userId == this.props.userId ? (
@@ -48,12 +49,21 @@ class EditProfile extends PureComponent {
           </AccountIformation>
         </div> */}
 
-        {this.props.profile.id ? <AccountForm
-          onSubmit={this.submeteHandler}
-          inputs={this.inputsArguments}
-          initialState={this.props.profile}
-        /> : <Loader/>}
+        <div className="edit-profile__form">
+          {this.props.profile.id ? <AccountForm
+            onSubmit={this.submeteHandler}
+            inputs={this.inputsArguments}
+            initialState={this.props.profile}
+          /> : <Loader />}
+        </div>
 
+        <div className="edit-profile__img-bg-wrapper">
+          <img
+            className="edit-profile__img-bg"
+            src={bgImg}
+            alt="hee should be img"
+          />
+        </div>
       </div>
     );
   }
