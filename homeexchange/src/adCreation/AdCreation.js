fb0ac35951737a5ea33a4d918d75d84be17a981c
Siarhei Kharlap <sergeygoodgood@gmail.com>
2020-12-16 17:45:51 +0300

    add bg img for ad creation

--- homeexchange/src/adCreation/AdCreation.js
+++ homeexchange/src/adCreation/AdCreation.js
@@ -1,31 +1,44 @@
 import React, { PureComponent } from "react";
 import AdForm from "../shared/components/adForm/AdForm";
 import { createNewAd } from "../shared/redux/ad/adActionCreator";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import Loader from "../shared/components/Loader/Loader";
-
+import bgImg from "../shared/imgs/ad-creat.svg";
+import "./ad-creation.scss";
 class AdCreation extends PureComponent {
-  submitHandler(state){
+  submitHandler(state) {
     console.log(state);
     this.props.createNewAd(state);
   }
   render() {
     return (
-      <div>
-        {this.props.isLoading 
-        ? <Loader/>
-        :<AdForm onSubmit={this.submitHandler.bind(this)} ></AdForm>}    
+      <div className="ad-creation">
+        <div className="ad-creation__form">
+          {this.props.isLoading ? (
+            <Loader />
+          ) : (
+            <AdForm onSubmit={this.submitHandler.bind(this)}></AdForm>
+          )}
+        </div>
+
+        <div className="ad-creation__img-bg-wrapper">
+          <img
+            className="ad-creation__img-bg"
+            src={bgImg}
+            alt="hee should be img"
+          />
+        </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  isLoading: state.loader.adForm
-})
+  isLoading: state.loader.adForm,
+});
 
 const mapDispatchToProps = {
-  createNewAd
-}
+  createNewAd,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(AdCreation);
