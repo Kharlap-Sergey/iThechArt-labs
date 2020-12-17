10839a2c7988b802775f16eeca1b868d6742aa1e
snovis <sergeygoodgood@gmail.com>
2020-12-17 09:32:40 +0300

    add styles for ad editor

--- homeexchange/src/adEditor/AdEditor.js
+++ homeexchange/src/adEditor/AdEditor.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { getAd } from './../shared/redux/ad/adActionCreator';
 import AdForm from './../shared/components/adForm/AdForm';
 import { updateAd } from './redux/update';
+import bgImg from "../shared/imgs/repairing.svg"
+import "./ad-editor.scss";
 class AdEditor extends PureComponent {
   constructor(props) {
     super(props);
@@ -10,7 +12,7 @@ class AdEditor extends PureComponent {
     this.state = { adId: this.props.match.params.id }
   }
   submitHandler(state) {
-    console.log("state",state)
+    console.log("state", state)
     this.props.updateAd(state);
     console.log(state);
   }
@@ -22,9 +24,17 @@ class AdEditor extends PureComponent {
   render() {
     console.log('render')
     return (
-      <div>
-        {this.props.ad.id && <AdForm onSubmit={this.submitHandler.bind(this)} initial={this.props.ad}></AdForm>}
-
+      <div className="ad-editor">
+        <div className="ad-editor__form">
+          {this.props.ad.id && <AdForm onSubmit={this.submitHandler.bind(this)} initial={this.props.ad}></AdForm>}
+        </div>
+        <div className="ad-editor__img-bg-wrapper">
+          <img
+            className="ad-editor__img-bg"
+            src={bgImg}
+            alt="hee should be img"
+          />
+        </div>
       </div>
     );
   }
