de28115e08a82204ebf296b8ef96bf45fb4786b9
Siarhei Kharlap <sergeygoodgood@gmail.com>
2020-12-23 10:07:26 +0300

    clere private route

--- homeexchange/src/shared/components/privateRoute/PrivateRoute.js
+++ homeexchange/src/shared/components/privateRoute/PrivateRoute.js
@@ -1,7 +1,6 @@
 import { path } from "./../../utils/path";
-import { Route, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 function PrivateRoute({ children, authed }) {
-  console.log('children', children)
   return authed == true ? children : <Redirect to={path.sign} />;
 }
 
