f7257ea533610b497dcc0170fc7eb887b4e81b1b
snovis <sergeygoodgood@gmail.com>
2021-01-03 15:50:54 +0300

    add models for pagging

---
+++ homeexchange/App/Api/Models/Page.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeexchangeApi.Models
+{
+    public sealed class Page<T>
+    {
+        public IEnumerable<T> Content { set; get; }
+        public PagingInfo Info { set; get; }
+    }
+}
