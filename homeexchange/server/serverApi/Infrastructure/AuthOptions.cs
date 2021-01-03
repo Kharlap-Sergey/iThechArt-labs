23c1ed69b5f7139791e8fd5abd693147ca2d7701
snovis <sergeygoodgood@gmail.com>
2021-01-03 13:18:31 +0300

    change some files

--- homeexchange/server/serverApi/Infrastructure/AuthOptions.cs
+++ homeexchange/server/serverApi/Infrastructure/AuthOptions.cs
@@ -12,7 +12,7 @@ namespace HomeexchangeApi.Infrastructure
         public const string ISSUER = "MyAuthServer"; // издатель токена
         public const string AUDIENCE = "MyAuthClient"; // потребитель токена
         const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
-        public const int LIFETIME = 10; // время жизни токена - 10 минута
+        public const int LIFETIME = 30; // время жизни токена - 10 минута
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
