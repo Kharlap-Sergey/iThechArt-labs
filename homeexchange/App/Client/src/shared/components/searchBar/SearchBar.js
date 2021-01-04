3855ea9060090a2388303458b15f79a45834beba
snovis <sergeygoodgood@gmail.com>
2021-01-05 00:34:41 +0300

    fix search bar

--- homeexchange/App/Client/src/shared/components/searchBar/SearchBar.js
+++ homeexchange/App/Client/src/shared/components/searchBar/SearchBar.js
@@ -1,5 +1,7 @@
 import React from "react";
+import PropTypes from 'prop-types'
 import "./search-bar.scss";
+
 function SearchBar({ onChange, val, onSearchClick, isOpen }) {
   const show = val?.length > 0;
   return (
@@ -15,9 +17,13 @@ function SearchBar({ onChange, val, onSearchClick, isOpen }) {
         value={val}
         onChange={(e) => onChange(e)}
       />
-      <div className="search" onClick={onSearchClick}></div>
+      <div className="search" onClick={onSearchClick} />
     </div>
   );
 }
 
+SearchBar.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  val: PropTypes.string.isRequired,
+}
 export default SearchBar;
