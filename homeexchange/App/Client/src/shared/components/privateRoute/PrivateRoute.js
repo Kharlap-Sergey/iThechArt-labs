a326d0314349dc1d870f0b690342c2c37acef19e
snovis <sergeygoodgood@gmail.com>
2021-01-05 00:37:30 +0300

    fix privateroute

--- homeexchange/App/Client/src/shared/components/privateRoute/PrivateRoute.js
+++ homeexchange/App/Client/src/shared/components/privateRoute/PrivateRoute.js
@@ -1,4 +1,4 @@
-import { path } from "./../../utils/path";
+import { path } from "shared/utils/path";
 import { Redirect, withRouter } from "react-router-dom";
 function PrivateRoute({ component: Component, authed, ...rest }) {
   return authed == true ? <Component {...rest} /> : <Redirect to={path.sign} />;
