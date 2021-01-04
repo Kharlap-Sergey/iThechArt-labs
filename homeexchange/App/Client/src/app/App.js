5e615d9782e5f2cf9b6b2fa203f926f12b7963c5
snovis <sergeygoodgood@gmail.com>
2021-01-04 13:17:44 +0300

    add selector for user

--- homeexchange/App/Client/src/app/App.js
+++ homeexchange/App/Client/src/app/App.js
@@ -2,23 +2,25 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "react-redux-toastr/lib/css/react-redux-toastr.min.css";
+import NavBar from "nav-bar/NavBar";
+import Home from "home/Home";
+import Profile from "profile/Profile";
+import Ad from "ad/Ad";
+import EditProfile from "editProfile/EditProfile";
+import AdCreation from "adCreation/AdCreation";
+import AdEditor from "adEditor/AdEditor";
+import Sign from "sign/Sign";
+import Chats from "chats/Chats";
+import { path } from "shared/utils/path";
+import RouteCatcher from "shared/components/routeCatcher/RouteCatcher";
+import SearchBar from "shared/components/searchBar/SearchBar";
+import PrivateRoute from "shared/components/privateRoute/PrivateRoute";
+import { selectUser } from "shared/redux/account/selectors";
 import "./app.scss";
-import NavBar from "../nav-bar/NavBar";
-import Home from "../home/Home";
-import Profile from "../profile/Profile";
-import Ad from "../ad/Ad";
-import RouteCatcher from "../shared/components/routeCatcher/RouteCatcher";
-import EditProfile from "../editProfile/EditProfile";
-import AdCreation from "../adCreation/AdCreation";
-import AdEditor from "../adEditor/AdEditor";
-import { path } from "../shared/utils/path";
-import Sign from "../sign/Sign";
-import Chats from "../chats/Chats";
-import SearchBar from "../shared/components/searchBar/SearchBar";
-import PrivateRoute from "../shared/components/privateRoute/PrivateRoute";
+
 function App() {
-  const user = useSelector((state) => state.user);
-  const auth = new Boolean(user.userId);
+  const user = useSelector((state) => selectUser(state));
+  const auth = !!user.userId;
   return (
     <div className="app">
       <Router>
@@ -26,7 +28,6 @@ function App() {
         <div className="main">
           <Switch>
             <RouteCatcher>
-              <Route exact path={"/test"} component={SearchBar} />
               <Route exact path={path.profile.edit()}>
                 <PrivateRoute authed={auth} component={EditProfile} />
               </Route>
