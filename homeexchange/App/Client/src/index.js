3100aff979506b452cdab5d0d36594b606e7eaea
snovis <sergeygoodgood@gmail.com>
2021-01-06 00:41:16 +0300

    import normalize from scss

--- homeexchange/App/Client/src/index.js
+++ homeexchange/App/Client/src/index.js
@@ -1,4 +1,3 @@
-import "normalize.css";
 import React from "react";
 import thunk from "redux-thunk";
 import ReactDOM from "react-dom";
@@ -12,7 +11,7 @@ import { applyMiddleware, compose, createStore } from "redux";
 export const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk)
     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
