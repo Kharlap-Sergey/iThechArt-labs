798d40ff6a222d848f9aacbe65021458a0e281c1
snovis <sergeygoodgood@gmail.com>
2021-01-05 11:40:44 +0300

    fix

--- homeexchange/App/Client/src/shared/redux/profile/actions.js
+++ homeexchange/App/Client/src/shared/redux/profile/actions.js
@@ -1,3 +1,9 @@
+import {
+  PROFILE_GET_BY_ID,
+  PROFILE_GET_ADS_FOR_PROFILE,
+  PROFILE_CLEAR,
+} from "./constants";
+
 export function setProfileByIdAction(user) {
   return {
     type: PROFILE_GET_BY_ID,
--- homeexchange/App/Client/src/shared/redux/profile/thunkActions.js
+++ homeexchange/App/Client/src/shared/redux/profile/thunkActions.js
@@ -2,6 +2,7 @@ import { pathApi } from "shared/utils/path";
 import { requestWrapper } from "shared/utils/requestWrapper";
 import { toastrNotifier } from "shared/redux/tostrNotifier";
 import { disableAllAction, enableRegistrationActin } from "shared/redux/loader/actions";
+import { setProfileByIdAction } from './actions';
 
 export function getProfileById(userId) {
   return async (dispatch) => {
