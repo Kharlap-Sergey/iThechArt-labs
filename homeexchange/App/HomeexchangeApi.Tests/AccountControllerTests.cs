ca2f8ef0af989713c1d2ad7faafe95b96df81460
snovis <sergeygoodgood@gmail.com>
2021-01-06 14:18:12 +0300

    add project for testing

---
+++ homeexchange/App/HomeexchangeApi.Tests/AccountControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using HomeexchangeApi.Models;
+using System;
+using Xunit;
+
+namespace HomeexchangeApi.Tests
+{
+    public class AccountControllerTests
+    {
+        [Fact]
+        public void Some()
+        {
+            User user = new User();
+
+            Assert.Equal(1, 1);
+        }
+
+    }
+}
