a5f49c9de14d43656182182da8de1e6859d75ef0
snovis <sergeygoodgood@gmail.com>
2021-01-08 18:40:28 +0300

    add tests for ad selectors

---
+++ homeexchange/App/Client/src/tests/shared/components/redux/ad/selectors.test.js
@@ -0,0 +1,35 @@
+import * as selectors from 'shared/redux/ad/selectors';
+
+describe("test ad selectors", () => {
+  let mockState
+  const mockAd = {
+    id: 1,
+    title: "some",
+    discription: "some",
+    dateOfPublication: Date.now().toString(),
+    type: 1,
+    userId: 1,
+  };
+  beforeEach(()=>{
+    mockState = {
+      ads:{
+        ad: mockAd,
+        adsPage: {
+          hasNext: false,
+          hasPrevious: false,
+          ads:[]
+        }
+      }
+    }
+  })
+
+  it("test selection ad", ()=>{
+    const expectedAd = mockState.ads.ad;
+    expect(selectors.selectAd(mockState)).toBe(expectedAd);
+  })
+
+  it("test selection adsPage", ()=>{
+    const expectedAdsPage = mockState.ads.adsPage;
+    expect(selectors.selectAdsPage(mockState)).toBe(expectedAdsPage);
+  })
+});
