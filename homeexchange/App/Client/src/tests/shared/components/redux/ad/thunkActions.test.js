e67f3542a7b054ad9f53ba916027a676d4835ad0
snovis <sergeygoodgood@gmail.com>
2021-01-08 20:24:33 +0300

    add tests for ad thunk actions for get ad method

--- homeexchange/App/Client/src/tests/shared/components/redux/ad/thunkActions.test.js
+++ homeexchange/App/Client/src/tests/shared/components/redux/ad/thunkActions.test.js
@@ -3,22 +3,43 @@ import thunk from "redux-thunk";
 import * as thunkActions from "shared/redux/ad/thunkActions";
 import fetchMock from "fetch-mock";
 import { path, pathApi } from "shared/utils/path";
-import { disableAllAction, enableAdFromtActin } from "shared/redux/loader/actions";
+import {
+  disableAllAction,
+  enableAdFromtActin,
+} from "shared/redux/loader/actions";
 import { redirectToAction } from "shared/redux/redirect/actions";
+import { setAdAction } from "shared/redux/ad/actions";
 
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
-
 
 describe("test thunk actions", () => {
   const mockAd = {
     id: 1,
     title: "some",
     discription: "some",
-    dateOfPublication: Date.now().toString(),
+    dateOfPublication: 1610126126734,
     type: 1,
     userId: 1,
   };
+  const mockAds = [
+    {
+      id: 1,
+      title: "some",
+      discription: "some",
+      dateOfPublication: 1610126126734,
+      type: 1,
+      userId: 1,
+    },
+    {
+      id: 2,
+      title: "some 1",
+      discription: "some 1",
+      dateOfPublication: 1610126122734,
+      type: 1,
+      userId: 1,
+    },
+  ];
   const initialStore = {
     ads: {
       ad: mockAd,
@@ -40,17 +61,14 @@ describe("test thunk actions", () => {
       headers: { "content-type": "application/json" },
     });
 
-    const expectActions = [
-      enableAdFromtActin(),
-      disableAllAction(),
-    ]
+    const expectActions = [enableAdFromtActin(), disableAllAction()];
     const store = mockStore(initialStore);
 
-    store.dispatch(thunkActions.createNewAd(mockAd)).then(()=>{
+    store.dispatch(thunkActions.createNewAd(mockAd)).then(() => {
       expect(store.getActions()).toBe(expectActions);
     });
   });
-  
+
   it("test create new ad to be success", () => {
     fetchMock.postOnce(pathApi.ad.create, {
       body: { text: "some" },
@@ -61,10 +79,50 @@ describe("test thunk actions", () => {
       enableAdFromtActin(),
       redirectToAction(path.home),
       disableAllAction(),
-    ]
+    ];
     const store = mockStore(initialStore);
 
-    store.dispatch(thunkActions.createNewAd(mockAd)).then(()=>{
+    store.dispatch(thunkActions.createNewAd(mockAd)).then(() => {
+      expect(store.getActions()).toBe(expectActions);
+    });
+  });
+
+  it("test get ad to be success", () => {
+    const mockAdId = 2;
+    fetchMock.getOnce(pathApi.ad.get(mockAdId), {
+      body: { text: mockAds[1] },
+      headers: { "content-type": "application/json" },
+    });
+
+    const expectActions = [
+      enableAdFromtActin(),
+      setAdAction(mockAds[1]),
+      disableAllAction(),
+    ];
+
+    const store = mockStore(initialStore);
+
+    store.dispatch(thunkActions.getAd(mockAdId)).then(() => {
+      expect(store.getActions()).toBe(expectActions);
+    });
+  });
+
+  it("test get ad to be error", () => {
+    const mockAdId = 2;
+    fetchMock.getOnce(pathApi.ad.get(mockAdId), {
+      status: 404,
+      body: { text: mockAds[1] },
+      headers: { "content-type": "application/json" },
+    });
+
+    const expectActions = [
+      enableAdFromtActin(),
+      disableAllAction(),
+    ];
+
+    const store = mockStore(initialStore);
+
+    store.dispatch(thunkActions.getAd(mockAdId)).then(() => {
       expect(store.getActions()).toBe(expectActions);
     });
   });
