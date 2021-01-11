f224b46beb340a48ffbcd3649b807b28a1d7b113
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-01-11 17:36:36 +0300

    Revert "delete useless"
    This reverts commit 06b6b47dfbe0b89c775dfe9b7ddcaf947c66a421.

---
+++ homeexchange/App/Homeexchange.Tests/AccountControllerTests.cs
@@ -0,0 +1,16 @@
+using HomeexchangeApi.Controllers;
+using System;
+using Xunit;
+
+namespace Homeexchange.Tests
+{
+    public class AccountControllerTests
+    {
+        [Fact]
+        public void LoginTryTest()
+        {
+            //
+            Assert.Equal(1, 2);
+        }
+    }
+}
