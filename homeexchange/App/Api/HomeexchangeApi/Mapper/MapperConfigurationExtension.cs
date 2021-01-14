eb37cb24833b610f0e4ad2bf3c14b6ea91ec9c0f
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-01-14 11:31:35 +0300

    Add: automapper frame

---
+++ homeexchange/App/Api/HomeexchangeApi/Mapper/MapperConfigurationExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace Homeexchange.Api.Mapper
+{
+    using AutoMapper;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class MapperConfigurationExtension
+    {
+        public static void AddAutomapper(this IServiceCollection services)
+        {
+            var mapperConfig = new MapperConfiguration(mc =>
+             {
+                 mc.AddProfile(new MappingProfile());
+             });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+        }
+    }
+}
